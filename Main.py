aabb30de01fc4227286d4d113443823f59e4c919
amne51ac <th3amne51ac@gmail.com>
2014-07-31 18:38:20 -0500

    Serial
    Begin work with Serial to initialize connection to drone

--- Main.py
+++ Main.py
@@ -1,1 +1,28 @@
 '''amne51ac and n1tr0'''
+import pymavlink, serial
+import serial.tools.list_ports
+
+class Drones():
+    
+    def comports():
+        a = []
+        b = serial.tools.list_ports.comports()
+        
+        while True:
+            try:
+                a.append(b.next())
+            except:
+                break
+        
+        return a
+
+    def connect():
+        '''
+        ser = serial.Serial('/dev/ttyS1', 19200, timeout=1)
+        ser.write('')
+        x = ser.read()
+        s = ser.read(10)
+        line = ser.readline()
+        
+        ser.close()
+        '''
