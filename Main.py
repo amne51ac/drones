b8aca423a37e4e47f84424e6f1a054c87714a5f3
amne51ac <th3amne51ac@gmail.com>
2014-07-31 21:23:17 -0500

    Links and notes

--- Main.py
+++ Main.py
@@ -2,27 +2,22 @@
 import pymavlink, serial
 import serial.tools.list_ports
 
-class Drones():
-    
-    def comports():
-        a = []
-        b = serial.tools.list_ports.comports()
-        
-        while True:
-            try:
-                a.append(b.next())
-            except:
-                break
-        
-        return a
 
-    def connect():
-        '''
-        ser = serial.Serial('/dev/ttyS1', 19200, timeout=1)
-        ser.write('')
-        x = ser.read()
-        s = ser.read(10)
-        line = ser.readline()
-        
-        ser.close()
-        '''
+def comports():
+    a = []
+    b = serial.tools.list_ports.comports()
+    
+    while True:
+        try:
+            a.append(b.next())
+        except:
+            break
+    
+    return a
+
+def connect():
+    ser = serial.Serial(0, 57600, timeout=10)
+    ser.write('')
+    ser.read()
+    ser.readline()
+    ser.close()
