5c41647b53fe62968d78680dedcd1e3f1a42c0ab
amne51ac <th3amne51ac@gmail.com>
2014-08-06 16:39:59 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -21,3 +21,40 @@ def connect():
     ser.read()
     ser.readline()
     ser.close()
+
+'''
+#CODE FOR PHONE APP
+#FOR PYTHONISTA
+# coding: utf-8
+
+import ui, location, socket
+
+HOST = '172.20.10.7'
+PORT = 50007
+
+location.start_updates()
+
+def button_action(sender):
+	loc = location.get_location()
+	label1 = sender.superview['label1']
+	mode1 = sender.superview['mode1']
+	if mode1.selected_index:
+		label1.text = 'Drop package'
+	else:
+		label1.text = 'Place package'
+	loc['mode'] = mode1.selected_index
+	ret = transmit(loc)
+	if ret:
+		label1.text = str(ret)
+		sender.enabled = False
+	
+def transmit(data):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((HOST, PORT))
+	s.sendall(str(data))
+	return s.recv(1024)
+	pass
+
+
+ui.load_view('Drones').present('popover')
+'''
