cd19b23d2117b39a1655373f76a68304f11a573c
nboutaleb <nboutale@hawk.iit.edu>
2015-06-30 18:03:56 -0500

    update
    compiled

--- quadrant.ino
+++ quadrant.ino
@@ -1,4 +1,4 @@
-#incluce <math.h>
+#include <math.h>
 int NWPin = 0;
 int NEPin = 1;
 int SEPin = 2;
@@ -13,6 +13,8 @@ int intensity;
 int acardinal;
 int cardinal;
 int truecard;
+const float pi = 3.14159265359;
+
 
 void setup() {
 Serial.begin(9600);
@@ -27,41 +29,40 @@ SWval=analogRead(SWPin);
 Y = ((NEval + NWval)/2) - ((SEval + SWval)/2);
 X = ((NEval + SEval)/2) - ((NWval + SWval)/2);
 
-if X==0
-acardinal=90;
+if (X==0)
+{acardinal=90;}
 else
-intensity = pow((pow(X,2)+pow(Y,2)),0.5);
-acardinal = abs(atan(Y/X));
+{intensity = pow((pow(X,2)+pow(Y,2)),0.5);
+acardinal = abs(atan(Y/X));}
 
-if Y > 0 && X > 0
-cardinal = acardinal * (180 / pi);
-      else if Y > 0 && X < 0
-      cardinal = (pi - acardinal) * (180 / pi);
-      else if Y < 0 && X < 0
-      cardinal = (pi + acardinal) * (180 / pi);
-      else if Y < 0 && X > 0
-      cardinal = (pi * 2 - acardinal) * (180 / pi);
-      else if X == 0 && Y > 0
-      cardinal = 90;
-      else if X == 0 && Y < 0
-      cardinal = 270;
-      else if Y == 0 && X > 0
-      cardinal = 0;
-      else if Y == 0 && X < 0
-      cardinal =180;
-      else if Y == 0 && X == 0
-      intensity = 0;
-      cardinal = 90;
-end
+if (Y > 0 && X > 0)
+{cardinal = acardinal * (180 / pi);}
+      else if (Y > 0 && X < 0)
+      {cardinal = (pi - acardinal) * (180 / pi);}
+      else if (Y < 0 && X < 0)
+      {cardinal = (pi + acardinal) * (180 / pi);}
+      else if (Y < 0 && X > 0)
+      {cardinal = ( pi * 2 - acardinal) * (180 / pi);}
+      else if (X == 0 && Y > 0)
+      {cardinal = 90;}
+      else if (X == 0 && Y < 0)
+      {cardinal = 270;}
+      else if (Y == 0 && X > 0)
+      {cardinal = 0;}
+      else if (Y == 0 && X < 0)
+      {cardinal =180;}
+      else if (Y == 0 && X == 0)
+      {intensity = 0;
+      cardinal = 90;}
 
 truecard = (-(cardinal-90)) % 360;
 
-Serial.print(NWval)
-Serial.print(NEval)
-Serial.print(SEval)
-Serial.print(SWval)
-Serial.print(acardinal)
-Serial.print(cardinal)
-Serial.print(truecard)
+Serial.print(NWval);
+Serial.print(NEval);
+Serial.print(SEval);
+Serial.print(SWval);
+Serial.print(acardinal);
+Serial.print(cardinal);
+Serial.print(truecard);
 
 }
