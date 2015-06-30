065f8fd11dbac0c9c7ca841022ff0c32bdea61c1
nboutaleb <nboutale@hawk.iit.edu>
2015-06-30 17:27:16 -0500

    Python to Arduino conversion
    Obtaining Magnetic Compass reading from photo-resistor values
    Input no included

--- quadrant.ino
+++ quadrant.ino
@@ -1,1 +1,67 @@
+#incluce <math.h>
+int NWPin = 0;
+int NEPin = 1;
+int SEPin = 2;
+int SWPin = 3;
+int NWval;
+int NEval;
+int SEval;
+int SWval;
+int Y;
+int X;
+int intensity;
+int acardinal;
+int cardinal;
+int truecard;
 
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+NWval=analogRead(NWPin);
+NEval=analogRead(NEPin);
+SEval=analogRead(SEPin);
+SWval=analogRead(SWPin);
+
+Y = ((NEval + NWval)/2) - ((SEval + SWval)/2);
+X = ((NEval + SEval)/2) - ((NWval + SWval)/2);
+
+if X==0
+acardinal=90;
+else
+intensity = pow((pow(X,2)+pow(Y,2)),0.5);
+acardinal = abs(atan(Y/X));
+
+if Y > 0 && X > 0
+cardinal = acardinal * (180 / pi);
+      else if Y > 0 && X < 0
+      cardinal = (pi - acardinal) * (180 / pi);
+      else if Y < 0 && X < 0
+      cardinal = (pi + acardinal) * (180 / pi);
+      else if Y < 0 && X > 0
+      cardinal = (pi * 2 - acardinal) * (180 / pi);
+      else if X == 0 && Y > 0
+      cardinal = 90;
+      else if X == 0 && Y < 0
+      cardinal = 270;
+      else if Y == 0 && X > 0
+      cardinal = 0;
+      else if Y == 0 && X < 0
+      cardinal =180;
+      else if Y == 0 && X == 0
+      intensity = 0;
+      cardinal = 90;
+end
+
+truecard = (-(cardinal-90)) % 360;
+
+Serial.print(NWval)
+Serial.print(NEval)
+Serial.print(SEval)
+Serial.print(SWval)
+Serial.print(acardinal)
+Serial.print(cardinal)
+Serial.print(truecard)
+
+}
